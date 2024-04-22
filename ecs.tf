f9f02f0595268a843f6869564dea3eab77d9e64a
27abdullah <98884418+27abdullah@users.noreply.github.com>
2024-04-22 04:17:54 +0000

    prac

--- ecs.tf
+++ ecs.tf
@@ -65,16 +65,9 @@ resource "aws_security_group" "todo" {
   description = "TaskOverflow Security Group"
 
   ingress {
-    from_port = 6400
-    to_port = 6400
-    protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port = 22
-    to_port = 22
-    protocol = "tcp"
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
